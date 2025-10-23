2aa0bcd56bf44d15798de2b238d82cb00a6e3b89
uday <udaysubba2004@gmail.com>
2025-10-23 22:20:17 +0530

    fix: users model typo

--- app/db/models/Email_model.py
+++ app/db/models/Email_model.py
@@ -5,7 +5,7 @@ class Email(Base):
     __tablename__ = "emails"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("user.user_id"))
+    user_id = Column(Integer, ForeignKey("users.user_id"))
     message_id = Column(String, unique=True, index=True)
     thread_id = Column(String, nullable=True)
     sender = Column(String, nullable=True)
